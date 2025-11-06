6069cc2c4008697c05fc766ab93a1fccd4d2737b
Solange Devos <angedespretz@gmail.com>
2025-11-06 11:58:41 +0100

    update movie

--- write/manip_data.py
+++ write/manip_data.py
@@ -14,6 +14,7 @@ CSV_PATH = os.path.join(BASE_DIR, 'data', 'movies.csv')
 
 def add_movie():
     current_year = datetime.datetime.now().year
+    new_movie = None
     
     try :
         title = str(input("Saisisser le titre du film :"))
@@ -42,16 +43,53 @@ def add_movie():
         print(e)
     else:
         print("Saisie correcte !")
+        new_movie = Movie(Movie.id, title, year, genre, age)
+        
     finally:
         print("Fin de l'ajout !")
-
-    new_movie = Movie(Movie.id, title, year, genre, age)
 
 
     return new_movie
 
 
+def update_movie():
+    current_year = datetime.datetime.now().year
+    movie_id = int(input("Saisissez l'id du film à modifier :"))
+    if id in df['id'].values:
+        try :
+            title = str(input("Saisisser le titre du film :"))
+            if title.isdigit():
+                raise Invalid_title("Le titre saisie est incorrect")
+        
+            year = int(input("Saisissez l'année de production :"))
+            if year <= 1900 or year > current_year:
+                raise Invalid_year("L'année de production saisie est incorrecte")
+            
+            genre = str(input("Saisissez le genre du film :"))
+            if genre.isdigit():
+                raise Invalid_genre("Le genre saisi est incorrect")
+            
+            age = int(input("Saisissez l'âge limite du film :"))
+            if age <0 or age > 18 :
+                raise Invalid_age_limit("L'âge saisi est incorrect")
 
+        except Invalid_title as e:
+            print(e)
+        except Invalid_year as e:
+            print(e)
+        except Invalid_genre as e:
+            print(e)
+        except Invalid_age_limit as e:
+            print(e)
+        else:
+            print("Saisie correcte !")
+            df.loc[df['id'] == movie_id, ['title','year','genre','age']] = [title, year, genre, age]
+            df.to_csv(CSV_PATH, index=False)
+        finally:
+            print("Fin de l'ajout !")
+    
+    else : 
+        print("Id non trouvé !")
 
 
 
